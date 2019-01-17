e4861ac730d3c58f366034a58ce942f7f10e4a32
WladSF <wpaivasf@gmail.com>
2019-01-16 19:30:57 -0800

    Added content on views, models, controllers

--- models/stuffer.js
+++ models/stuffer.js
@@ -1,24 +1,27 @@
+// Contains boilerplate code to handle user input from front end (which is sent through stufferController.js)
+// and sends it to orm so request can be understood by MySQL. MySQL is then read/updated accordingly.
+// Model defines what data the app should contain, it updates the app to reflect added item.
+
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var stuffer = {
   all: function(cb) {
-    orm.all("items", function(res) {
+    orm.owned("items", function(res) {    //Posts to 'items' table
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("items", cols, vals, function(res) {
+    orm.borrowed("items", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("items", objColVals, condition, function(res) {
+    orm.available("items", objColVals, condition, function(res) {
       cb(res);
     });
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions to the controller (stufferController.js).
 module.exports = stuffer;
