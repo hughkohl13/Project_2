0baad20dd070b87221908916d6da0d620b2c026c
Hugh Kohl <hgkohl@us.ibm.com>
2019-01-22 19:00:39 -0800

    updating user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,28 @@
 var orm = require("../config/orm.js");
 
-var user = {
+var users = {
   all: function(cb) {
-    orm.all("users", function(res) {    
+    orm.all("users", function(res) {    //Posts to 'users' table
       cb(res);
     });
   },
-  create: function(cols, vals, cb) {                     //Posts to 'users' table
+  available: function(cb) {
+    orm.available("users", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  borrowed: function(cb) {
+    orm.borrowed("users", function(res) {
+      cb(res);
+    })
+  },
+  create: function(cols, vals, cb) {
     orm.create("users", cols, vals, function(res) {
       cb(res);
+    });
+  },
+  delete: function(cols, vals, cb) {
+    orm.delete("users", cols, vals, function(res) {
     });
   },
   update: function(objColVals, condition, cb) {
@@ -16,6 +30,11 @@ var user = {
       cb(res);
     });
   },
+  owned: function(cb) {
+    orm.owned("users", function(res){
+      cb(res);
+    })
+  }
 };
 
 // Export the database functions to the controller (user.js).
