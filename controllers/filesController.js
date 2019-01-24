70b6685294009567c9496d6abf5fb29fa7f36514
gillesleroy <gleroy69@me.com>
2019-01-24 11:31:42 -0800

    Gilles: modified filescontroller and index.html

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -3,20 +3,9 @@ var path = require("path");
 // var app = express();
 var router = express.Router();
 
+// Front end Javascript
 router.get("/lib", function(req,res) {
   res.sendFile(path.join(__dirname, "./../public/assets/js/lib.js"));
-});
-router.get("/css/reset", function(req,res) {
-  res.sendFile(path.join(__dirname, "./../public/assets/css/reset.css"));
-});
-router.get("/css/styles", function(req,res) {
-  res.sendFile(path.join(__dirname, "./../public/assets/css/styles.css"));
-});
-router.get("/image/concrete", function(req,res) {
-  res.sendFile(path.join(__dirname, "./../public/assets/images/concrete_seamless.png"));
-});
-router.get("/image/stuffer", function(req,res) {
-  res.sendFile(path.join(__dirname, "./../public/assets/images/Stuffer_Logo.png"));
 });
 router.get("/js/stuffer", function(req,res) {
   res.sendFile(path.join(__dirname, "./../public/assets/js/Stuffer.js"));
@@ -26,6 +15,30 @@ router.get("/js/postitem", function(req,res) {
 });
 router.get("/js/index", function(req,res) {
   res.sendFile(path.join(__dirname, "./../public/assets/js/index.js"));
+});
+// css
+router.get("/css/reset", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/css/reset.css"));
+});
+router.get("/css/styles", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/css/styles.css"));
+});
+
+// Images
+router.get("/image/stuffer", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/images/Stuffer_Logo_Main.png"));
+});
+router.get("/image/lending", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/images/Stuffer_characters/Lending.png"));
+});
+router.get("/image/borrowing", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/images/Stuffer_characters/Borrowing.png"));
+});
+router.get("/image/sharing", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/images/Stuffer_characters/Sharing.png"));
+});
+router.get("/image/concrete", function(req,res) {
+  res.sendFile(path.join(__dirname, "./../public/assets/images/concrete_seamless.png"));
 });
 
 module.exports = router;
