4d7aa01d1d94e5baeb5a83b607a1e2c93a6b5361
gillesleroy <gleroy69@me.com>
2019-01-24 19:11:17 -0800

    Gilles: added signup logic with users addition

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,4 +10,25 @@ $(document).ready(function() {
     // location.address("/postitem/page");
     location.replace("/borrowitem/page");
   });
+  $("#button-signup").on("click", function(){
+    // alert("Clicked signup");
+    // location.address("/postitem/page");
+    location.replace("/signup/page");
+  });
+  window.onload = function(){
+    if(localStorage.userId) {
+        // this will only work if the token is set in the localStorage
+        // $("h3").text("Welcome Back " + localStorage.first);
+        $("#greetingOne").text("Welcome to");
+         $("#greeting").attr("class","header2");
+        $("#greetingTwo").text(localStorage.first);
+        // $("#greeting").text("Welcome to Stuffer "+localStorage.first);
+
+        // $("#input-username").hide();
+        // $("#input-firstname").hide();
+        // $("#input-lastname").hide();
+        // $("label").hide();
+        // $("#button-submit").hide();
+    }
+   }  
 });
